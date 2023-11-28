cd9bf1f3f06080272601733d3590d99b4e90b091
Thomas <@TOMMY>
2023-11-28 17:39:07 -0600

    Claw, Gantry, and Lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Drone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Drone.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-import static org.firstinspires.ftc.teamcode.hardware.RobotConfig.DRONE_ROTATION_UP;
 import static org.firstinspires.ftc.teamcode.hardware.RobotConfig.DRONE_ROTATION_UP_NAME;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -8,14 +7,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Drone {
 
-    private Servo droneUpServo;
+    private Servo droneLaunchServo;
 
     public Drone(HardwareMap hardwareMap) {
-        this.droneUpServo = hardwareMap.get(Servo.class, DRONE_ROTATION_UP_NAME);
+        this.droneLaunchServo = hardwareMap.get(Servo.class, DRONE_ROTATION_UP_NAME);
     }
 
     public void raise() {
-        this.droneUpServo.setPosition(0.2);
+        this.droneLaunchServo.setPosition(0.2);
 
     }
 }
